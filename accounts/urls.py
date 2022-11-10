3c0971c12e979c12623d268a022e1cc5329fc00d
Ricardo Luis Barbosa Pereira <patresio@gmail.com>
2022-11-10 10:43:51 -0300

    crm OK

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('customer/', views.customer, name='customer'),
+    path('customer/<str:pk_test>', views.customer, name='customer'),
     path('products/', views.products, name='products'),
 ]
--- crmDjango/settings.py
+++ crmDjango/settings.py
@@ -94,9 +94,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -106,7 +106,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

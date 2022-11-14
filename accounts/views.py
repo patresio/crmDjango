4c23230361140e254e4a7ac50e231cc513d93d06
patresio <patresio@gmail.com>
2022-11-14 17:29:43 -0300

    teste

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
-# Create your views here.
 from .models import *
 from .forms import *
 from .filters import *

691143039f6f38058dfd32750d3f14e4eb963e92
Ricardo Luis Barbosa Pereira <patresio@gmail.com>
2022-11-22 10:41:47 -0300

    Aplicação OK

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
@@ -19,6 +22,23 @@ urlpatterns = [
     path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'),
     #products
     path('create_product/', views.createProduct, name='create_product'),
+
+    #Reset password
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), 
+        name="password_reset_complete"),
 ]
 
 '''
--- crmDjango/settings.py
+++ crmDjango/settings.py
@@ -134,3 +134,12 @@ MESSAGE_TAGS = {
     constants.INFO: 'alert-info',
     constants.WARNING: 'alert-warning'
 }
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'patresio@gmail.com'
+EMAIL_HOST_PASSWORD = 'Tatuzinho13'
